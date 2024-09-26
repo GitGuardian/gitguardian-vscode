377ca7c831551d9922b0dff214fd7a19982993d3
Salomé Voltz <salome.voltz@gitguardian.com>
2024-09-26 16:18:34 +0200

    fix(remediation-message): convert dashboard to api

--- src/utils.ts
+++ src/utils.ts
@@ -2,6 +2,10 @@ import * as vscode from "vscode";
 import { spawnSync } from "child_process";
 import path = require("path");
 
+const reDashboard = "dashboard";
+const reApi = "api";
+const gitGuardianDomains = ["gitguardian.tech", "gitguardian.com"];
+
 export async function isGitInstalled(): Promise<boolean> {
   return new Promise((resolve) => {
     let proc = spawnSync("git", ["--version"]);
@@ -20,5 +24,23 @@ export function getCurrentFile(): string {
     return activeEditor.document.fileName;
   } else {
     return "";
+  }
+}
+
+export function dasboardToApi(dashboardUrl: string): string {
+  const domainMatch = gitGuardianDomains.some((domain) => dashboardUrl.includes(domain));
+  if (domainMatch) {
+    return dashboardUrl.replace(reDashboard, reApi);
+  } else {
+    return dashboardUrl;
+  }
+}
+
+export function apiToDashboard(apiUrl: string): string {
+  const domainMatch = gitGuardianDomains.some((domain) => apiUrl.includes(domain));
+  if (domainMatch) {
+    return apiUrl.replace(reApi, reDashboard);
+  } else {
+    return apiUrl;
   }
 }
