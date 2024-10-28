47ce4df766f1fa48720b920e9ae5670381953656
Jonathan Griffe <jonathan.griffe@gitguardian.com>
2024-10-28 12:09:53 +0100

    test: move ggshield-api.test.ts to lib folder to match code folder structure

--- src/test/suite/lib/ggshield-api.test.ts
+++ src/test/suite/lib/ggshield-api.test.ts
@@ -3,13 +3,14 @@ import * as statusBar from "../../../gitguardian-interface/gitguardian-status-ba
 import * as simple from "simple-mock";
 import { diagnosticCollection, scanFile } from "../../../lib/ggshield-api";
 import * as runGGShield from "../../../lib/run-ggshield";
-import path = require("path");
 import { Uri, window } from "vscode";
-import assert = require("assert");
 import {
   scanResultsNoIncident,
   scanResultsWithIncident,
 } from "../../constants";
+import * as assert from "assert";
+import { ExtensionContext, Memento } from "vscode";
+import { ggshieldAuthStatus } from "../../../lib/ggshield-api";
 
 suite("scanFile", () => {
   let updateStatusBarMock: simple.Stub<Function>;
@@ -89,5 +90,67 @@ suite("scanFile", () => {
     // The error message is displayed
     assert.strictEqual(errorMessageMock.callCount, 1);
     assert.strictEqual(errorMessageMock.lastCall.args[0], "ggshield: Error\n");
+  });
+});
+
+suite("ggshieldAuthStatus", function () {
+  let isAuthenticated: boolean;
+  let mockGlobalState: Memento & {
+    setKeysForSync(keys: readonly string[]): void;
+  };
+  let mockContext: Partial<ExtensionContext>;
+  let runGGShieldMock: simple.Stub<Function>;
+
+  setup(function () {
+    isAuthenticated = false;
+
+    mockGlobalState = {
+      get: (key: string) =>
+        key === "isAuthenticated" ? isAuthenticated : undefined,
+      update: (key: string, value: any) => {
+        if (key === "isAuthenticated") {
+          isAuthenticated = value;
+        }
+        return Promise.resolve();
+      },
+      keys: () => [],
+      setKeysForSync: (keys: readonly string[]) => {},
+    };
+
+    mockContext = {
+      globalState: mockGlobalState,
+    };
+    runGGShieldMock = simple.mock(runGGShield, "runGGShieldCommand");
+  });
+  teardown(function () {
+    simple.restore();
+  });
+
+  test("Valid authentication should update isAuthenticated to true", async function () {
+    runGGShieldMock.returnWith({
+      status: 0,
+      stdout: '{"detail": "Valid API key.", "status_code": 200}',
+      stderr: "",
+    });
+
+    await ggshieldAuthStatus(
+      {} as GGShieldConfiguration,
+      mockContext as ExtensionContext
+    );
+    assert.strictEqual(isAuthenticated, true);
+  });
+
+  test("Invalid authentication should keep isAuthenticated to false", async function () {
+    runGGShieldMock.returnWith({
+      status: 0,
+      stdout: '{"detail": "Invalid API key.", "status_code": 401}',
+      stderr: "",
+    });
+
+    await ggshieldAuthStatus(
+      {} as GGShieldConfiguration,
+      mockContext as ExtensionContext
+    );
+    assert.strictEqual(isAuthenticated, false);
   });
 });
